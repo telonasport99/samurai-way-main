5b206870b0d068e6b186b79ed11a75e7cabf9a64
telonasport99 <telonasport99@gmail.com>
2022-10-11 21:26:19 +0300

    13 lesson

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+    return( <header className='header'>
+            <img src="https://cdn-icons-png.flaticon.com/512/447/447057.png"/>
+        </header>
+
+    )
+}
+export default Header
