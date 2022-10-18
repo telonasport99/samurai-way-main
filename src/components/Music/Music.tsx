bb71fffdc5627fb39aac20df9ed2b4bc3dd1b5ac
telonasport99 <telonasport99@gmail.com>
2022-10-18 22:03:37 +0300

    23 lesson

---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dialogs from "../Dialogs/Dialogs";
+function News(){
+    return(<div>News
+    </div>
+    )
+}
+export default News;
---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Dialogs from "../Dialogs/Dialogs";
+function Music(){
+    return(<div>Music
+    </div>
+    )
+}
+export default Music;
